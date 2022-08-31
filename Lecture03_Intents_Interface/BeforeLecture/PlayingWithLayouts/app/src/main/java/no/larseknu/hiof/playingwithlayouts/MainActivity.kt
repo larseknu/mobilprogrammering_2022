628e3d4f25aa1dd6924e3479acf904b4ad399d00
Lars Emil Knudsen <larseknu@hiof.no>
2022-08-31 09:24:50 +0200

    Cleanup of BeforeLecture - Layouts

--- Lecture03_Intents_Interface/BeforeLecture/PlayingWithLayouts/app/src/main/java/no/larseknu/hiof/playingwithlayouts/MainActivity.kt
+++ Lecture03_Intents_Interface/BeforeLecture/PlayingWithLayouts/app/src/main/java/no/larseknu/hiof/playingwithlayouts/MainActivity.kt
@@ -1,17 +1,11 @@
-package no.hiof.larseknu.playingwithlayouts
+package no.larseknu.hiof.playingwithlayouts
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
-import kotlinx.android.synthetic.main.activity_contraint_layout_view_gone.*
-
 
 class MainActivity : AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        setContentView(R.layout.activity_constraint_layout_basics)
     }
-
 }
